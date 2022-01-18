5760327f5512dd085378c19e63766d287954079f
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2022-01-18 23:57:06 +0800

    cleanup files

--- porter_bot_description/build/porter_bot_description/CMakeFiles/porter_bot_description_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/porter_bot_description_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/porter_bot_description_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
